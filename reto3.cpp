716a1b9f6871440ee5585db55becd3828c27f5e6
RubenCF2505 <rubencallejasf01@gmail.com>
2025-12-06 13:05:31 +0100

    planteamiento algoritmo

---
+++ reto3.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+using namespace std;
+
+class BinTree
+{
+    struct nodo
+    {
+        char etiqueta;
+        nodo *padre;
+        nodo *hijoIzq;
+        nodo *hijoDer;
+    };
+
+    nodo raiz;
+
+public:
+    BinTree() {}
+    BinTree(string cadena)
+    {
+        if (cadena.length() > 0)
+        {
+            raiz.etiqueta = cadena[0];
+            raiz.hijoDer = nullptr;
+            raiz.hijoIzq = nullptr;
+            raiz.padre = nullptr;
+
+            cadena.erase(0, 1); 
+            auto it = cadena.begin();
+            auto it2 = cadena.begin();
+            nodo *actual = &raiz;
+
+            while (it != cadena.end())
+            {
+
+                if (*it != '#')
+                {
+                    crearHijoIzq(actual, *it);
+                    actual = actual->hijoIzq;
+
+                    ++it;
+                    ++it2;
+                }
+                else
+                {
+                    ++it2;
+                    if (it2 != cadena.end() && *it2 != '#')
+                    {
+                        crearHijoDer(actual, *it2);
+                        actual = actual->hijoDer;
+
+                        ++it;
+                        ++it2;
+                    }
+                    else
+                    {
+                        actual = actual->padre;
+
+                        if (it2 != cadena.end())
+                            ++it2;
+                        ++it;
+                    }
+                }
+
+            }
+        }
+    }
+
+    void inorden(nodo *actual)
+    {
+        if (actual == nullptr)
+            return;
+
+        inorden(actual->hijoIzq);
+        cout << actual->etiqueta << " "; 
+        inorden(actual->hijoDer);        
+    }
+
+    void mostrarInorden()
+    {
+        inorden(&raiz);
+        cout << endl;
+    }
+    void crearHijoIzq(nodo *padre, char etiqueta)
+    {
+        nodo *nuevo = new nodo;
+        nuevo->etiqueta = etiqueta;
+        nuevo->padre = padre;
+        padre->hijoIzq = nuevo;
+        nuevo->hijoIzq = nullptr;
+        nuevo->hijoDer = nullptr;
+    }
+    void crearHijoDer(nodo *padre, char etiqueta)
+    {
+        nodo *nuevo = new nodo;
+        nuevo->etiqueta = etiqueta;
+        nuevo->padre = padre;
+        padre->hijoDer = nuevo;
+        nuevo->hijoIzq = nullptr;
+        nuevo->hijoDer = nullptr;
+    }
+};
+void leerEntrada(string &cadena)
+{
+    BinTree arbol(cadena);
+    arbol.mostrarInorden();
+}
+int main(int argc, char **args)
+{
+    string cadena = args[1];
+    leerEntrada(cadena);
+    return 0;
+}
